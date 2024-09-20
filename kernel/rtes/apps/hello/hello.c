d93a6b74b720326e7991bb741222a5d269c00a60
Michael Crotty <mcrotty@andrew.cmu.edu>
2024-09-20 12:51:03 -0400

    Some setup stuff

---
+++ kernel/rtes/apps/hello/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(int argc, char *argv) {
+	printf("Hello, world! It's warm and cozy here in user-space");
+
+	return 0;
+}
---
+++ kernel/rtes/modules/hello/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv) {
+	printf("Hello, world! Kernel-space -- the land of the free and the home of the brave.");
+
+	return 0;
+}
