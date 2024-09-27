4d34bd97f6cabfd16bb62ef0cea177a16e1e1cb1
Ethan <echerian@andrew.cmu.edu>
2024-09-27 13:43:24 -0400

    Committing Hello world files

---
+++ rtes/kernel/hello.c
@@ -0,0 +1,13 @@
+// Simple C program to display "Hello World"
+
+// Header file for input output functions
+#include <linux/kernel.h>  // Needed for printk
+
+// Main function: entry point for execution
+int main(void) {
+
+    // writing print statement to print hello world
+    //printk(KERN_INFO "Hello, world! Kernel-space -- the land of the free and the home of the brave.");
+
+    return 0;
+}
