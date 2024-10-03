8e7996daa5580b28d0c917720898ca189f66a879
Ethan Cherian <echerian@andrew.cmu.edu>
2024-10-03 01:35:05 -0400

    Got working character device driver for processor

--- rtes/modules/psdev/psdev.c
+++ rtes/modules/psdev/psdev.c
@@ -6,9 +6,13 @@
 #include <linux/uaccess.h>
 #include <linux/fs.h> 
 #include <linux/slab.h>
+#include <linux/rcupdate.h>
+#include <linux/sched.h>
+//#include <linux/sched/signal.h>
+#include <linux/rcupdate.h>  // For RCU lockin
 
 
-#define BUFFER_SIZE    1024
+#define BUFFER_SIZE    4096
 //File Operation 
 static int psdev_open(struct inode *inode, struct file *file);
 static int psdev_release(struct inode *inode, struct file *file);
@@ -30,14 +34,14 @@ static ssize_t psdev_write(struct file *file, const char __user *buf, size_t cou
 struct psdev_device_data{
     struct cdev cdev;
     int is_open;  //Flag to indicate when the device is open
-    size_t buffer_size;
-    size_t  fileSize;
-    char *buffer;
-
 };
 
 
-static struct psdev_device_data *characterDriverDevice;
+static struct psdev_device_data characterDriverDevice;
+
+// sysfs class structure
+//static struct class *psdevdev_class = NULL;
+
 dev_t devNumber;
 
 static int psdev_open(struct inode *inode, struct file *file)
@@ -53,14 +57,13 @@ static int psdev_open(struct inode *inode, struct file *file)
     }
     
     //allocate memory for the buffer
-    device->buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
-    if (!device->buffer) {
-        printk(KERN_INFO "psdev: Failed to allocate memory for buffer\n");
-        return -ENOMEM;  // Return an error if buffer allocation fails
-    }
+    // device->buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+    // if (!device->buffer) {
+    //     printk(KERN_INFO "psdev: Failed to allocate memory for buffer\n");
+    //     return -ENOMEM;  // Return an error if buffer allocation fails
+    // }
 
-    device->buffer_size = BUFFER_SIZE;
-    device->fileSize = 0;  
+  
     device->is_open = 1;
     file->private_data = device;
     printk(KERN_INFO "Device opened\n");
@@ -72,25 +75,65 @@ static int psdev_release(struct inode *inode, struct file *file)
 {
     struct psdev_device_data *device = container_of(inode->i_cdev, struct psdev_device_data, cdev);
     device->is_open = 0;
-    kfree(device->buffer);
+    // //kfree(device->buffer);
     printk(KERN_INFO "Device closed\n");
     return 0;
 }
 
 static long psdev_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-    return -1;
+    return -ENOTSUPP;
 }
 
 static ssize_t psdev_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
 {
-    struct psdev_device_data *device = file->private_data;
-    size_t remainingSize = device->fileSize - *offset;
+    struct task_struct *task;
+    ssize_t length = 0;
+    char *buffer; 
+    size_t remainingSize = 0;
     ssize_t readSize = count;
-    
-    //check to see if at the end of file
+
+    buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+    if(!buffer)
+    {
+        return -ENOMEM;
+    }
+
+    length = length + snprintf(buffer+length,BUFFER_SIZE-length, "tid\tpid\tpr\tname \n");
+
+    //lock to read the task list
+    //__rcu_read_lock();
+
+    //
+    for_each_process(task)
+    {
+        if(task->rt_priority > 0)
+        {
+            length = length + snprintf(buffer+length,BUFFER_SIZE-length, "%d\t%d\t%d\t%s \n",
+                                    task->tgid, task->pid,task->rt_priority, task->comm);
+        }
+
+        
+    }
+
+    if(length < BUFFER_SIZE)
+    {
+        buffer[length] = '\0';
+    }
+    else
+    {
+        buffer[BUFFER_SIZE-1] = '\0';
+    }
+    //unlock since done reading the task list
+    //__rcu_read_unlock();
+
+    //copy only specific points from buffer
+    remainingSize = length - *offset;
+
+    //check to see if at the end of file or there is no data
     if(remainingSize <= 0)
     {
+        kfree(buffer);
         return 0;
     }
 
@@ -99,77 +142,71 @@ static ssize_t psdev_read(struct file *file, char __user *buf, size_t count, lof
     {
         readSize = remainingSize;
     }
-    
 
     // Copy data from kernel space (device buffer) to user space
-    if (copy_to_user(buf, device->buffer + *offset, readSize)) {
+    if(copy_to_user(buf, buffer + *offset, readSize))
+    {
         return -EFAULT;  // Return error if copy fails
     }
 
     //increment the offset
     *offset +=readSize;
-
+    kfree(buffer);
     return readSize;
 }
 
 static ssize_t psdev_write(struct file *file, const char __user *buf, size_t count, loff_t *offset)
 {   
-    struct psdev_device_data *device = file->private_data;
-    size_t remainingSize = device->buffer_size - device->fileSize;
-    ssize_t writeSize = count;
-    
-    //check to see if there enough space
-    if(count > remainingSize)
-    {
-        writeSize = remainingSize;
-    }
 
-    // Copy data from user space to the device buffer
-    if (copy_from_user(device->buffer + device->fileSize, buf, writeSize)) {
-        return -EFAULT;
-    }
-    
-
-    device->fileSize += writeSize;
-
-    return writeSize;
+    return -ENOTSUPP;
 }
 
 
-
-static int psdev_init(void)
+//Device Node 
+static int __init psdev_init(void)
 {
     int allocRet;
-
+    printk(KERN_INFO "Hello World 1!");
     //allocate major number
     allocRet = alloc_chrdev_region(&devNumber, 0,1,"psdev");
 
+    printk(KERN_INFO "Data is %d %d\n",allocRet, devNumber);
     if(allocRet != 0)
     {
         return allocRet;
     }
 
-     //Initialize the device
-    cdev_init(&characterDriverDevice->cdev, &psdev_fops);
-    characterDriverDevice->is_open = 0;
-
+    //Initialize the device
+    cdev_init(&characterDriverDevice.cdev, &psdev_fops);
+    
+    characterDriverDevice.is_open = 0;
+    
     //Add the chacater driver
-    cdev_add(&characterDriverDevice->cdev,devNumber,1);
+    cdev_add(&characterDriverDevice.cdev,devNumber,1);
 
-
+    //psdevdev_class = class_create(THIS_MODULE, "psdev");
+    //device_create(psdevdev_class, NULL, devNumber, NULL, "psdev");
+    
     printk(KERN_INFO "Character device registered with major number %d\n", MAJOR(devNumber));
+   
+    
     return 0;
     
 }
 
-static void psdev_exit(void)
+static void __exit psdev_exit(void)
 {
-    cdev_del(&characterDriverDevice->cdev);
+    //device_destroy(psdevdev_class,devNumber);
+    //class_unregister(psdevdev_class);
+    //class_destroy(psdevdev_class);
+    cdev_del(&characterDriverDevice.cdev);
+    unregister_chrdev_region(devNumber, 1);
+
     printk(KERN_INFO "Character device unregistered %d\n", MAJOR(devNumber));
 }
 
 
 
-MODULE_LICENSE("Dual BSD/GPL");
+//MODULE_LICENSE("Dual BSD/GPL");
 module_init(psdev_init);
 module_exit(psdev_exit);
