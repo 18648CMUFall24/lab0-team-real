8f3776d1a59fb945c6d161f606891dd09bd61e53
Ethan Cherian <echerian@andrew.cmu.edu>
2024-10-07 12:45:20 -0400

    initial implementation of cleanup initialization for overrriding syscall

--- rtes/modules/cleanup/cleanup.c
+++ rtes/modules/cleanup/cleanup.c
@@ -1,14 +1,73 @@
 #include <linux/init.h>
 #include <linux/module.h>
-MODULE_LICENSE("Dual BSD/GPL");
+#include <linux/mm.h>
+#include <linux/mman.h>
+#include <linux/syscalls.h>
+#include <linux/unistd.h>
+
+
+unsigned long *syscall_table = NULL;
+
+asmlinkage long (*orginal_sys_exit_group)(int error_code);
+
+
+asmlinkage long temp_sys_exit_group(int error_code)
+{
+
+
+    return orginal_sys_exit_group(error_code);
+}
+
+
+static unsigned long **find_syscall_table(void)
+{
+  unsigned long int offset = PAGE_OFFSET;
+  unsigned long **sct;
+
+  while (offset < ULLONG_MAX) {
+    sct = (unsigned long **)offset;
+
+    if (sct[__NR_close] == (unsigned long *) sys_close)
+      return sct;
+
+    offset += sizeof(void *);
+  }
+
+  return NULL;
+}
+
 static int cleanup_init(void)
 {
- printk(KERN_ALERT "Hello, world! Kernel-space -- the land of the free and home of the brave.\n");
- return 0;
+    syscall_table = (void **)find_syscall_table();
+    if(syscall_table == NULL)
+    {
+        printk(KERN_ERR "Syscall table not found!\n");
+        return -1;
+    }
+
+    write_cr0(read_cr0() & ~0x00010000);
+    orginal_sys_exit_group = (void *)syscall_table[__NR_exit_group];
+
+    syscall_table[__NR_exit_group] = (unsigned long *)temp_sys_exit_group;    
+    write_cr0(read_cr0() | 0x00010000);
+
+    printk(KERN_ALERT "Cleanuo: Module loaded!.\n");
+    return 0;
 }
 static void cleanup_exit(void)
 {
- printk(KERN_ALERT "Goodbye, cruel world\n");
+    if(syscall_table == NULL)
+    {
+        printk(KERN_ERR "Syscall table not found!\n");
+        return;
+    }
+
+    write_cr0(read_cr0() & ~0x00010000);
+    syscall_table[__NR_exit_group] = orginal_sys_exit_group;
+    write_cr0(read_cr0() | 0x00010000);
+
+    printk(KERN_ALERT "Cleanuo: Module unloaded!.\n");
 }
 module_init(cleanup_init);
 module_exit(cleanup_exit);
+MODULE_LICENSE("Dual BSD/GPL");
