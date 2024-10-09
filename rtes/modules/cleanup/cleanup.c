65c1615b4dba00fb8d233f28da7b9f14f3041135
Ethan Cherian <echerian@andrew.cmu.edu>
2024-10-09 03:57:57 -0400

    was able to get sloppyapp process and see if its file is still open

--- rtes/modules/cleanup/cleanup.c
+++ rtes/modules/cleanup/cleanup.c
@@ -7,6 +7,9 @@
 #include <linux/highmem.h>
 #include <linux/kernel.h>
 #include <linux/kallsyms.h>
+#include <linux/fdtable.h>
+#include <linux/fs.h>
+#include <linux/string.h>
 
 #include <asm/unistd.h>
 #include <asm/pgtable.h>
@@ -17,11 +20,37 @@ asmlinkage long (*original_sys_exit_group)(int error_code);
 
 extern unsigned long kallsyms_lookup_name(const char *name);
 
+static char *comm = NULL;
+module_param(comm, charp, 0644);
 
 //Temporary System exit group call
 asmlinkage long temp_sys_exit_group(int error_code)
 {
-    printk(KERN_ALERT "Entered the temp sys call!\n");
+    struct task_struct *task = current;
+    // struct fdtable *fdt;
+    // struct file *file;
+    // int fd;
+
+    if(comm && strstr(task->comm, comm))
+    {
+        struct fdtable *fdt = files_fdtable(task->files);
+        int fd;
+        int filesFound = 0;
+        /**/
+        spin_lock(&task->files->file_lock);
+        for(fd = 0; fd < fdt->max_fds; fd++)
+        {
+            if(fdt->fd[fd] && !filesFound)
+            {
+                printk("cleanup: process %s (PID %d) did not close files:\n", task->comm, task->pid);
+                filesFound = 1;
+            }
+        }
+        
+    }
+
+    //printk(KERN_ALERT "Entered the temp sys call %s !\n", task->comm)
+    
     return original_sys_exit_group(error_code);
 }
 
@@ -41,7 +70,7 @@ static int cleanup_init(void)
     // Cast function pointer to unsigned long when assigning to syscall_table
     syscall_table[__NR_exit_group] = (unsigned long)temp_sys_exit_group;
 
-    printk(KERN_ALERT "Cleanup: Module loaded!\n");
+    printk(KERN_ALERT "Cleanup: Module loaded with parameter %s!\n", comm);
     return 0;
 }
 
