59920e36bac0b3df9e8732a7a613910e95ff16b0
Ethan Cherian <echerian@andrew.cmu.edu>
2024-10-25 18:48:10 -0400

    Got set reserve building

--- rtes/kernel/reservation.c
+++ rtes/kernel/reservation.c
@@ -1,11 +1,65 @@
 #include <linux/rtes_framework.h>
 #include <linux/errno.h>
 #include <linux/syscalls.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
 
+
+struct threadNode* threadHead = NULL;
 
 SYSCALL_DEFINE4(set_reserve, pid_t, tid, struct timespec*, C , struct timespec*, T , int, cpuid) 
 {
-	return 1;
+	struct threadNode *new_node;
+	struct timespec c,t;
+	struct cpumask cpumask;
+
+	if(cpuid > 0 && cpuid <= 3)
+	{
+		return EINVAL;
+	}
+
+	if(tid == 0)
+	{
+		tid = current->pid;
+	}
+
+	// Copy from user space
+    if (copy_from_user(&c, C, sizeof(struct timespec)) || copy_from_user(&t, T, sizeof(struct timespec))) {
+        return -EFAULT;
+    }
+
+	new_node = kmalloc(sizeof(struct threadNode), GFP_KERNEL);
+	if(!new_node)
+	{
+		return -ENOMEM;
+	}
+
+	//set the nodes with its parameters
+	new_node->C = c;
+    new_node->T = t;
+    new_node->tid = tid;
+    new_node->cpuid = cpuid;
+
+
+	// Setting up CPU affinity using syscall for sched_setaffinity
+	cpumask_clear(&cpumask);
+    cpumask_set_cpu(cpuid, &cpumask);
+
+	//bin TID against a specific CPU
+	if(sched_setaffinity(tid, &cpumask) == -1)
+	{
+		kfree(new_node);
+		return -1;
+	}
+
+
+	//setting the thread head and its next
+	new_node->next = threadHead;
+    threadHead = new_node; // Insert into linked list
+
+
+
+	return 0;
 }
 
 SYSCALL_DEFINE1(cancel_reserve, pid_t, tid) 
