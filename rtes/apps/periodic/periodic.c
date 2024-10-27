d2cfdd4901d80447a1a89770bd3ed08e59f77a7a
Michael Crotty <mcrotty@andrew.cmu.edu>
2024-10-27 13:51:13 -0400

    Over engineered periodic

--- rtes/apps/periodic/periodic.c
+++ rtes/apps/periodic/periodic.c
@@ -7,15 +7,42 @@
 #include <sched.h>
 #include <stdint.h>
 #include <sys/syscall.h>
+#include <sys/time.h>
 
 const uint64_t CLOCKS_PER_MSEC = (uint64_t)CLOCKS_PER_SEC / 1000;
 const uint64_t CLOCKS_PER_NSEC = (uint64_t)CLOCKS_PER_SEC / 1000000000;
 
+void increment_by_period(struct timeval *time, uint64_t period_s, int64_t period_us) {
+    time->tv_sec += period_s;
+    time->tv_usec += period_us;
+    time->tv_sec += time->tv_usec / 1000000;
+    time->tv_usec %= 1000000;
+}
+
+// Returns a negative value if bad
+int set_sleep_duration(struct timespec *duration, struct timeval *wakeup) {
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    duration->tv_sec = wakeup->tv_sec - now.tv_sec;
+    duration->tv_nsec = wakeup->tv_usec - now.tv_usec;
+    
+    // I think this while only ever runs once, but just in case use while
+    while(duration->tv_nsec < 0) {
+        duration->tv_sec -= 1;
+        duration->tv_nsec += 1000000;
+    }
+
+    duration->tv_nsec *= 1000;
+
+    return ((duration->tv_sec >= 0) && (duration->tv_nsec >= 0)) ? 0 : -1;
+}
+
 // Main function: entry point for execution
 int main(int argc, char *argv[]) {
+    struct timespec sleep_time;
+    struct timeval next_wakeup;
+    gettimeofday(&next_wakeup, NULL);
 
-    uint64_t next_wakeup = clock();
-    
     //check if there is the same amount of arguments
     if (argc != 4) {
         printf("Usage: %s <C (ms)> <T (ms)> <cpuid>\n", argv[0]);
@@ -25,12 +52,24 @@ int main(int argc, char *argv[]) {
     int cArg = atoi(argv[1]);
     int tArg = atoi(argv[2]);
     int cpuArg = atoi(argv[3]);
-    struct timespec sleep_time;
-    uint64_t clocks_between_periods = tArg * CLOCKS_PER_MSEC;
+
+
+    if (tArg < cArg) {
+        perror("Period less than Execution Budget");
+        return -1;
+    }
+
+    uint64_t clocks_running = cArg * CLOCKS_PER_MSEC;
+    time_t period_s = tArg / 1000;
+    suseconds_t period_us = (tArg % 1000) * 1000;
+
+    printf("Cost: %d ms (%lld clock cycles)\n", cArg, clocks_running);
+    printf("Period: %d ms (%d s %d us)\n", tArg, period_s, period_us);
+    printf("Core: %d\n", cpuArg);
+
 
     // Setting up CPU affinity using syscall for sched_setaffinity
     unsigned long cpumask = 1UL << cpuArg;  // Set affinity to specified CPU
-
     if (syscall(__NR_sched_setaffinity, 0, sizeof(cpumask), &cpumask) == -1) {
         perror("sched_setaffinity syscall");
         return -1;
@@ -39,24 +78,26 @@ int main(int argc, char *argv[]) {
 
     while(1)
     {
-        next_wakeup += clocks_between_periods;
-        uint64_t start = clock();
-        uint64_t totalElpasedTime = 0;
+        increment_by_period(&next_wakeup, period_s, period_us);
+        clock_t start = clock();
+        clock_t clocks_periodElapsedTime = 0;
+
+        printf("Started period...");
 
         //pretend to do busy work
-        while(cArg >= totalElpasedTime)
+        while(clocks_running >= clocks_periodElapsedTime)
         {
-            totalElpasedTime = (clock() -  start ) / CLOCKS_PER_MSEC;
+            clocks_periodElapsedTime = (clock() - start);
         }
 
-        //set sleep time struct to go to sleep
-        uint64_t sleep_clock_cycles = next_wakeup - clock();
+        printf("Ended period %d\n", (clock() / CLOCKS_PER_SEC));
 
-        sleep_time.tv_sec =  sleep_clock_cycles / CLOCKS_PER_SEC;
-        sleep_time.tv_nsec = (sleep_clock_cycles % CLOCKS_PER_SEC) / CLOCKS_PER_NSEC;
+        if (set_sleep_duration(&sleep_time, &next_wakeup) < 0) {
+            printf("Overran period\n");
+            return -1;
+        }
 
         nanosleep(&sleep_time, NULL);
-
     }
 
     return 0;
